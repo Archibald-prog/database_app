19c15c0aa812da83357f293c0cb595e942a6f4ef
Archibald-prog <archpod@gmail.com>
2023-11-07 04:37:08 +0300

    Edited main menu list.

--- database_app/women/views.py
+++ database_app/women/views.py
@@ -1,18 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
-from .models import Women, Category
+from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
 
 
 def index(request):
     posts = Women.objects.all()
+    cats = Category.objects.all()
 
     context = {
         'menu': menu,
         'title': 'Главная страница',
-        'posts': posts
+        'cats': cats,
+        'posts': posts,
+        'cat_selected': 0,
     }
     return render(request, 'women/index.html', context)
 
@@ -25,5 +32,34 @@ def about(request):
     return render(request, 'women/about.html', context)
 
 
-def categories(request, catid):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>Категория: {catid}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context)
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
