01fc145048e3f2b90ea2056d6d82e9792847eb57
Archibald-prog <archpod@gmail.com>
2023-11-07 04:39:21 +0300

    Added 'category' url-pattern.

--- database_app/women/urls.py
+++ database_app/women/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),   # http://127.0.0.1:8000/
+    path('', index, name='home'),  # http://127.0.0.1:8000/
     path('about/', about, name='about'),  # http://127.0.0.1:8000/about/
-    path('addpage/', addpage, name='add_page'),
-    path('contact/', contact, name='contact'),
-    path('login/', login, name='login'),
-    path('post/<int:post_id>/', show_post, name='post'),
-    path('cats/<int:catid>/', categories),  # http://127.0.0.1:8000/cats/1/
+    path('addpage/', addpage, name='add_page'),  # http://127.0.0.1:8000/addpage/
+    path('contact/', contact, name='contact'),   # http://127.0.0.1:8000/contact/
+    path('login/', login, name='login'),  # http://127.0.0.1:8000/login/
+    path('post/<int:post_id>/', show_post, name='post'),   # http://127.0.0.1:8000/post/2/
+    path('cats/<int:cat_id>/', show_category, name='category'),   # http://127.0.0.1:8000/cats/2/
 ]
