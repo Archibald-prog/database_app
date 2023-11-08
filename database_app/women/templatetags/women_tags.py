4a7a2176f8e6139af243ec702f7e8618e63c525d
Archibald-prog <archpod@gmail.com>
2023-11-08 14:07:59 +0300

    Added filter to get_categories. Added show_categories.

--- database_app/women/templatetags/women_tags.py
+++ database_app/women/templatetags/women_tags.py
@@ -5,5 +5,17 @@ register = template.Library()
 
 
 @register.simple_tag()
-def get_categories():
-    return Category.objects.all()
+def get_categories(filter=None):
+    if filter is None:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
+
+
+@register.inclusion_tag('women/list_categories.html')
+def show_categories(cat_selected=0):
+    cats = Category.objects.all()
+    return {'cats': cats, 'cat_selected': cat_selected}
+
+
+
