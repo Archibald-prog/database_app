17463abfd998a815020c9f8c8de392faefa1f801
Archibald-prog <archpod@gmail.com>
2023-11-16 05:08:37 +0300

    Switched 'home', 'add_page', 'post' and 'category' url-patterns from functions to classes.

--- database_app/women/urls.py
+++ database_app/women/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),  # http://127.0.0.1:8000/
+    path('', WomenHome.as_view(), name='home'),  # http://127.0.0.1:8000/
     path('about/', about, name='about'),  # http://127.0.0.1:8000/about/
-    path('addpage/', addpage, name='add_page'),  # http://127.0.0.1:8000/addpage/
+    path('addpage/', AddPage.as_view(), name='add_page'),  # http://127.0.0.1:8000/addpage/
     path('contact/', contact, name='contact'),   # http://127.0.0.1:8000/contact/
     path('login/', login, name='login'),  # http://127.0.0.1:8000/login/
-    path('post/<slug:post_slug>/', show_post, name='post'),   # http://127.0.0.1:8000/post/madonna/
-    path('cats/<slug:cat_slug>/', show_category, name='category'),   # http://127.0.0.1:8000/cats/aktrisyi/
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),   # http://127.0.0.1:8000/post/madonna/
+    path('cats/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),   # http://127.0.0.1:8000/cats/aktrisyi/
 ]
