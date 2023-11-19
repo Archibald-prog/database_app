2034bdf91602e70ee6701c97b30c65ad842d8323
Archibald-prog <archpod@gmail.com>
2023-11-19 12:34:41 +0300

    Added DataMixin.

--- database_app/modules/services/utils.py
+++ database_app/modules/services/utils.py
@@ -1,5 +1,15 @@
 from uuid import uuid4
 from pytils.translit import slugify
+from django.db.models import Count
+
+from women import models
+
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
 
 
 def unique_slugify(instance, slug):
@@ -11,3 +21,19 @@ def unique_slugify(instance, slug):
     while model.objects.filter(slug=unique_slug).exists():
         unique_slug = f'{unique_slug}-{uuid4().hex[:4]}'
     return unique_slug
+
+
+class DataMixin:
+    def get_user_context(self, **kwargs):
+        context = kwargs
+        cats = models.Category.objects.annotate(Count('women'))
+
+        user_menu = menu.copy()
+        if not self.request.user.is_authenticated:
+            user_menu.pop(1)
+        context['menu'] = user_menu
+
+        context['cats'] = cats
+        if 'cat_selected' not in context:
+            context['cat_selected'] = 0
+        return context
